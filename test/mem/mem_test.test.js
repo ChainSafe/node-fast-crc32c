e0dad99145774d164b2b229c7e947c5ac1343c99
tuyennhv <tuyen@chainsafe.io>
2023-01-12 09:31:20 +0700

    Use @node-rs/crc32 v1.6.0 (#3)
    * perf: replace @node-rs/crc32 by sse4_crc32
    
    * Add mem test
    
    * Move benchmark to test folder
    
    * Use @node-rs/crc32 1.6.0

---
+++ test/mem/mem_test.test.js
@@ -0,0 +1,45 @@
+// Enable this to try sse4_crc32
+// const Sse4Crc32 = require("sse4_crc32");
+const nodeRs = require("@node-rs/crc32");
+const nodeRsCrc32c = nodeRs.crc32c;
+
+
+
+/**
+ * This is to prove the memory issue is resolved with @node-rs/crc32 1.6.0
+ * Refer to https://github.com/napi-rs/node-rs/issues/655
+ */
+async function memTest() {
+  const count = 1_000_000_000;
+  for (let i = 0; i < count; i++) {
+    const bytes = Buffer.alloc(1000);
+    nodeRsCrc32c(bytes);
+    // Sse4Crc32.calculate(bytes);
+    if (i % 100_000 === 0) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      const {heapTotal, rss} = process.memoryUsage();
+      console.log(
+        "Memory usage",
+        Math.floor((i * 100) / count) + "%",
+        "heapTotal",
+        toMem(heapTotal),
+        "rss",
+        toMem(rss)
+      );
+    }
+  }
+}
+
+function toMem(n) {
+  const bytes = Math.abs(n);
+  const sign = n > 0 ? "+" : "-";
+  if (bytes < 1e6) return sign + Math.floor(bytes / 10) / 100 + " KB";
+
+  if (bytes < 1e9) return sign + Math.floor(bytes / 1e4) / 100 + " MB";
+
+  return sign + Math.floor(bytes / 1e7) / 100 + " GB";
+}
+
+console.log("Start node-rs crc32c mem test");
+
+memTest().then(() => console.log("Done"));
---
+++ test/perf/benchmark.test.js
@@ -0,0 +1,43 @@
+const {itBench, setBenchOpts} = require("@dapplion/benchmark");
+const crypto = require("node:crypto");
+const nodeRsCrc32c = require('@node-rs/crc32').crc32c;
+const sse42Crc = require('sse4_crc32').sse42_crc;
+const tableCrc = require('sse4_crc32').table_crc;
+const jsCrc32c = require('../../impls/js_crc32c').calculate;
+const jsCrc32 = require('buffer-crc32').unsigned;
+
+const tests = [{
+  name: '@node-rs/crc32c',
+  calculate: nodeRsCrc32c,
+},
+{
+  name: 'sse4_crc32c_hw',
+  calculate: sse42Crc,
+}, {
+  name: 'sse4_crc32c_sw',
+  calculate: tableCrc,
+}, {
+  name: 'js_crc32c',
+  calculate: jsCrc32c,
+}, {
+  name: 'js_crc32',
+  calculate: jsCrc32,
+}];
+
+describe("crc32 implementations", function () {
+  setBenchOpts({
+    minMs: 10 * 1000,
+  })
+
+  const counts = [500, 1000, 10_000, 100_000, 1_000_000];
+  for (const c of counts) {
+    const bytes = crypto.randomBytes(c);
+
+    for (const {name, calculate} of tests) {
+      itBench({
+        id: `${name} ${c} bytes`,
+        fn: () => calculate(bytes),
+      })
+    }
+  }
+});
